cdd00d71480247be7d9a96554eb24ab9f67caa05
Allan Pinheiro <apinheiro@railz.ai>
2022-11-06 21:08:58 -0300

    update db info

--- exemplo-com-banco.js
+++ exemplo-com-banco.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const mysql = require('mysql2/promise');
+const express = require("express");
+const mysql = require("mysql2/promise");
 
 const app = express();
 const port = 8000;
@@ -7,69 +7,69 @@ const port = 8000;
 app.use(express.json());
 
 async function executarNoBanco(query) {
-    const conexao = await mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '1234',
-        database: 'dc'
-    });
+  const conexao = await mysql.createConnection({
+    host: "localhost",
+    user: "admin",
+    password: "12345678",
+    database: "dc",
+  });
 
-    const [results, ] = await conexao.execute(query);
+  const [results] = await conexao.execute(query);
 
-    return results;
+  return results;
 }
 
+app.get("/produtos", async (req, res) => {
+  let produtos = await executarNoBanco("SELECT * FROM produtos");
 
-app.get('/produtos', async (req, res) => {
-    let produtos = await executarNoBanco('SELECT * FROM produtos');
-
-    res.send(produtos);
+  res.send(produtos);
 });
 
-app.get('/produtos/:id', async (req, res) => {
-    let produto = await executarNoBanco('SELECT * FROM produtos WHERE id='+req.params.id);
-    
-    res.send(produto[0])
+app.get("/produtos/:id", async (req, res) => {
+  let produto = await executarNoBanco(
+    "SELECT * FROM produtos WHERE id=" + req.params.id
+  );
+
+  res.send(produto[0]);
 });
 
-app.delete('/produtos/:id', async (req, res) => {
-    let query = 'DELETE FROM produtos WHERE id='+req.params.id;
+app.delete("/produtos/:id", async (req, res) => {
+  let query = "DELETE FROM produtos WHERE id=" + req.params.id;
 
-    await executarNoBanco(query);
+  await executarNoBanco(query);
 
-    res.send(204);
-})
+  res.send(204);
+});
 
-app.post('/produtos', async (req, res) => {
-    const body = req.body;
+app.post("/produtos", async (req, res) => {
+  const body = req.body;
 
-    let query = `
+  let query = `
         INSERT INTO produtos 
             (nome, categoria, valor, tamanho)
         VALUES
             ('${body.nome}', '${body.categoria}', '${body.valor}', '${body.tamanho}')
     `;
 
-    const resultado = await executarNoBanco(query);
-    body.id = resultado.insertId; //recuperando o id que foi gerado e adicionando na resposta
+  const resultado = await executarNoBanco(query);
+  body.id = resultado.insertId; //recuperando o id que foi gerado e adicionando na resposta
 
-    res.send(body)
-
+  res.send(body);
 });
 
-app.patch('/produtos/:id', async (req, res) => {
-    let query = `
+app.patch("/produtos/:id", async (req, res) => {
+  let query = `
         UPDATE produtos SET 
             categoria = '${req.body.categoria}',
             tamanho = '${req.body.tamanho}'
         WHERE id = ${req.params.id}
     `;
 
-    await executarNoBanco(query);
+  await executarNoBanco(query);
 
-    res.send(req.body);
+  res.send(req.body);
 });
 
 app.listen(port, () => {
-    console.log("Servidor rodando em http://localhost:8000");
+  console.log("Servidor rodando em http://localhost:8000");
 });
