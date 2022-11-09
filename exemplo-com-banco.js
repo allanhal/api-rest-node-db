0496358c66a0cb9e025e366646b0b79028f60540
angelolustosa <>
2022-11-09 08:21:17 -0300

    sequelize

--- exemplo-com-banco.js
+++ exemplo-com-banco.js
@@ -12,7 +12,7 @@ const sequelize = new Sequelize('sys', process.argv[3], process.argv[4], {
 async function test(){
   try {
     await sequelize.createSchema('dc', { ifNotExists: true });
-    await sequelize.authenticate();
+    // await sequelize.authenticate();
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
